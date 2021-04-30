8bfef5fa74354bda15734991f0bd1e5e0f2f40ed
Julian Herrero <microweb10@gmail.com>
2021-04-30 13:05:42 +0700

    Fix python script example

--- lib/machine_learning/scripts/script.py
+++ lib/machine_learning/scripts/script.py
@@ -20,13 +20,17 @@ with open(os.getcwd() + "/lib/machine_learning/scripts/machine_learning_budget_i
 
 data = [
     {"commentable_id": 1, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 1"},
-    {"commentable_id": 2, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 1"},
-    {"commentable_id": 3, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 1"},
-    {"commentable_id": 4, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 1"},
+    {"commentable_id": 2, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 2"},
+    {"commentable_id": 3, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 3"},
+    {"commentable_id": 4, "commentable_type": "Proposal", "body": "Resumen de comentarios de la propuesta con ID 4"},
+    {"commentable_id": 1, "commentable_type": "Debate", "body": "Resumen de comentarios del debate con ID 1"},
+    {"commentable_id": 2, "commentable_type": "Debate", "body": "Resumen de comentarios del debate con ID 2"},
+    {"commentable_id": 3, "commentable_type": "Debate", "body": "Resumen de comentarios del debate con ID 3"},
+    {"commentable_id": 4, "commentable_type": "Debate", "body": "Resumen de comentarios del debate con ID 4"},
     {"commentable_id": 1, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 1"},
-    {"commentable_id": 2, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 1"},
-    {"commentable_id": 3, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 1"},
-    {"commentable_id": 4, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 1"}
+    {"commentable_id": 2, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 2"},
+    {"commentable_id": 3, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 3"},
+    {"commentable_id": 4, "commentable_type": "Budget::Investment", "body": "Resumen de comentarios del proyecto con ID 4"}
 ]
 
 with open(os.getcwd() + "/lib/machine_learning/scripts/machine_learning_comments_textrank.json", mode="w") as json_file:
