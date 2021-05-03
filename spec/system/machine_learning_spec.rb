63d9466e2a224a6fe7e9ea88e53416a3150193eb
Julian Herrero <microweb10@gmail.com>
2021-05-03 11:05:38 +0700

    Refactor specs

--- spec/system/machine_learning_spec.rb
+++ spec/system/machine_learning_spec.rb
@@ -2,8 +2,14 @@ require "rails_helper"
 
 describe "Machine learning" do
   let(:debate) { create(:debate) }
+  let(:related_debate) { create(:debate) }
   let(:proposal) { create(:proposal) }
-  let(:investment) { create(:budget_investment) }
+  let(:related_proposal) { create(:proposal) }
+  let(:budget) { create(:budget) }
+  let(:investment) { create(:budget_investment, budget: budget) }
+  let(:related_investment) { create(:budget_investment) }
+  let(:user_tag) { create(:tag, name: "user tag") }
+  let(:machine_learning_tag) { create(:tag, name: "machine learning tag") }
 
   before do
     Setting["feature.machine_learning"] = true
@@ -12,157 +18,200 @@ describe "Machine learning" do
     Setting["machine_learning.tags"] = false
   end
 
-  scenario "Show Machine Learning summary comments correctly" do
-    budget = create(:budget)
-    investment = create(:budget_investment, budget: budget)
-    ml_summary_comment_debate = create(:ml_summary_comment, commentable: debate)
-    ml_summary_comment_proposal = create(:ml_summary_comment, commentable: proposal)
-    ml_summary_comment_investment = create(:ml_summary_comment, commentable: investment)
+  describe "Summary comments" do
+    scenario "is displayed for debates if setting is enabled" do
+      ml_summary_comment_debate = create(:ml_summary_comment, commentable: debate)
 
-    visit debate_path(debate)
+      visit debate_path(debate)
 
-    within "#comments" do
-      expect(page).not_to have_content "Summary comments"
-      expect(page).not_to have_content "Summary of comments elaborated by Machine Learning"
-      expect(page).not_to have_content "#{ml_summary_comment_debate.body}"
+      within "#comments" do
+        expect(page).not_to have_content "Summary comments"
+        expect(page).not_to have_content "Summary of comments elaborated by Machine Learning"
+        expect(page).not_to have_content "#{ml_summary_comment_debate.body}"
+      end
+
+      Setting["machine_learning.summary_comments"] = true
+
+      visit debate_path(debate)
+
+      within "#comments" do
+        expect(page).to have_content "Summary comments"
+        expect(page).to have_content "Summary of comments elaborated by Machine Learning"
+        expect(page).to have_content "#{ml_summary_comment_debate.body}"
+      end
     end
 
-    visit proposal_path(proposal)
+    scenario "is displayed for proposals if setting is enabled" do
+      ml_summary_comment_proposal = create(:ml_summary_comment, commentable: proposal)
 
-    within "#comments" do
-      expect(page).not_to have_content "Summary comments"
-      expect(page).not_to have_content "Summary of comments elaborated by Machine Learning"
-      expect(page).not_to have_content "#{ml_summary_comment_proposal.body}"
+      visit proposal_path(proposal)
+
+      within "#comments" do
+        expect(page).not_to have_content "Summary comments"
+        expect(page).not_to have_content "Summary of comments elaborated by Machine Learning"
+        expect(page).not_to have_content "#{ml_summary_comment_proposal.body}"
+      end
+
+      Setting["machine_learning.summary_comments"] = true
+
+      visit proposal_path(proposal)
+
+      within "#comments" do
+        expect(page).to have_content "Summary comments"
+        expect(page).to have_content "Summary of comments elaborated by Machine Learning"
+        expect(page).to have_content "#{ml_summary_comment_proposal.body}"
+      end
     end
 
-    visit budget_investment_path(budget, investment)
+    scenario "is displayed for budget investments if setting is enabled" do
+      ml_summary_comment_investment = create(:ml_summary_comment, commentable: investment)
 
-    within "#tab-comments" do
-      expect(page).not_to have_content "Summary comments"
-      expect(page).not_to have_content "Summary of comments elaborated by Machine Learning"
-      expect(page).not_to have_content "#{ml_summary_comment_investment.body}"
-    end
+      visit budget_investment_path(budget, investment)
 
-    Setting["machine_learning.summary_comments"] = true
+      within "#tab-comments" do
+        expect(page).not_to have_content "Summary comments"
+        expect(page).not_to have_content "Summary of comments elaborated by Machine Learning"
+        expect(page).not_to have_content "#{ml_summary_comment_investment.body}"
+      end
 
-    visit debate_path(debate)
+      Setting["machine_learning.summary_comments"] = true
 
-    within "#comments" do
-      expect(page).to have_content "Summary comments"
-      expect(page).to have_content "Summary of comments elaborated by Machine Learning"
-      expect(page).to have_content "#{ml_summary_comment_debate.body}"
-    end
+      visit budget_investment_path(budget, investment)
 
-    visit proposal_path(proposal)
-
-    within "#comments" do
-      expect(page).to have_content "Summary comments"
-      expect(page).to have_content "Summary of comments elaborated by Machine Learning"
-      expect(page).to have_content "#{ml_summary_comment_proposal.body}"
-    end
-
-    visit budget_investment_path(budget, investment)
-
-    within "#tab-comments" do
-      expect(page).to have_content "Summary comments"
-      expect(page).to have_content "Summary of comments elaborated by Machine Learning"
-      expect(page).to have_content "#{ml_summary_comment_investment.body}"
-    end
-  end
-
-  scenario "Show Machine Learning related content correctly on debates" do
-    debate = create(:debate)
-    debate2 = create(:debate)
-
-    create(:related_content, parent_relationable: debate, child_relationable: debate2, machine_learning: true)
-
-    visit debate_path(debate)
-
-    within ".related-content" do
-      expect(page).to have_content "Related content (0)"
-      expect(page).not_to have_selector ".related-content-title"
-    end
-
-    Setting["machine_learning.related_content"] = true
-
-    visit debate_path(debate)
-
-    within ".related-content" do
-      expect(page).to have_content "Related content (1)"
-      expect(page).to have_selector(".related-content-title", count: 1)
-      expect(page).to have_content "#{debate2.title}"
+      within "#tab-comments" do
+        expect(page).to have_content "Summary comments"
+        expect(page).to have_content "Summary of comments elaborated by Machine Learning"
+        expect(page).to have_content "#{ml_summary_comment_investment.body}"
+      end
     end
   end
 
-  scenario "Show Machine Learning related content correctly on proposals" do
-    proposal = create(:proposal)
-    proposal2 = create(:proposal)
+  describe "Related content" do
+    scenario "is displayed for debates if setting is enabled" do
+      create(:related_content, parent_relationable: debate, child_relationable: related_debate, machine_learning: true)
 
-    create(:related_content, parent_relationable: proposal, child_relationable: proposal2, machine_learning: true)
+      visit debate_path(debate)
 
-    visit proposal_path(proposal)
+      within ".related-content" do
+        expect(page).to have_content "Related content (0)"
+        expect(page).not_to have_selector ".related-content-title"
+      end
 
-    within ".related-content" do
-      expect(page).to have_content "Related content (0)"
-      expect(page).not_to have_selector ".related-content-title"
+      Setting["machine_learning.related_content"] = true
+
+      visit debate_path(debate)
+
+      within ".related-content" do
+        expect(page).to have_content "Related content (1)"
+        expect(page).to have_selector(".related-content-title")
+        expect(page).to have_content "#{related_debate.title}"
+      end
     end
 
-    Setting["machine_learning.related_content"] = true
+    scenario "is displayed for proposals if setting is enabled" do
+      create(:related_content, parent_relationable: proposal, child_relationable: related_proposal, machine_learning: true)
 
-    visit proposal_path(proposal)
+      visit proposal_path(proposal)
 
-    within ".related-content" do
-      expect(page).to have_content "Related content (1)"
-      expect(page).to have_selector(".related-content-title", count: 1)
-      expect(page).to have_content "#{proposal2.title}"
+      within ".related-content" do
+        expect(page).to have_content "Related content (0)"
+        expect(page).not_to have_selector ".related-content-title"
+      end
+
+      Setting["machine_learning.related_content"] = true
+
+      visit proposal_path(proposal)
+
+      within ".related-content" do
+        expect(page).to have_content "Related content (1)"
+        expect(page).to have_selector(".related-content-title")
+        expect(page).to have_content "#{related_proposal.title}"
+      end
+    end
+
+    scenario "is displayed for budget investments if setting is enabled" do
+      create(:related_content, parent_relationable: investment, child_relationable: related_investment, machine_learning: true)
+      visit budget_investment_path(budget, investment)
+
+      within ".related-content" do
+        expect(page).to have_content "Related content (0)"
+        expect(page).not_to have_selector ".related-content-title"
+      end
+
+      Setting["machine_learning.related_content"] = true
+
+      visit budget_investment_path(budget, investment)
+
+      within ".related-content" do
+        expect(page).to have_content "Related content (1)"
+        expect(page).to have_selector(".related-content-title", count: 1)
+        expect(page).to have_content "#{related_investment.title}"
+      end
     end
   end
 
-  scenario "Show Machine Learning related content correctly on investments" do
-    budget = create(:budget)
-    investment = create(:budget_investment, budget: budget)
-    investment2 = create(:budget_investment, budget: budget)
+  describe "Tags" do
+    scenario "are displayed for debates if setting is enabled" do
+      create :ml_tag, tag: machine_learning_tag
+      debate.update! tag_list: [user_tag, machine_learning_tag]
 
-    create(:related_content, parent_relationable: investment, child_relationable: investment2, machine_learning: true)
-    visit budget_investment_path(budget, investment)
+      visit debate_path(debate)
 
-    within ".related-content" do
-      expect(page).to have_content "Related content (0)"
-      expect(page).not_to have_selector ".related-content-title"
+      within "#tags_debate_#{debate.id}" do
+        expect(page).to have_link "user tag"
+        expect(page).not_to have_link "machine learning tag"
+      end
+
+      Setting["machine_learning.tags"] = true
+
+      visit debate_path(debate)
+
+      within "#tags_debate_#{debate.id}" do
+        expect(page).to have_link "user tag"
+        expect(page).to have_link "machine learning tag"
+      end
     end
 
-    Setting["machine_learning.related_content"] = true
+    scenario "are displayed for proposals if setting is enabled" do
+      create :ml_tag, tag: machine_learning_tag
+      proposal.update! tag_list: [user_tag, machine_learning_tag]
 
-    visit budget_investment_path(budget, investment)
+      visit proposal_path(proposal)
 
-    within ".related-content" do
-      expect(page).to have_content "Related content (1)"
-      expect(page).to have_selector(".related-content-title", count: 1)
-      expect(page).to have_content "#{investment2.title}"
-    end
-  end
+      within "#tags_proposal_#{proposal.id}" do
+        expect(page).to have_link "user tag"
+        expect(page).not_to have_link "machine learning tag"
+      end
 
-  scenario "Show Machine Learning tags correctly" do
-    user_tag = Tag.create!(name: "user tag")
-    ml_tag = Tag.create!(name: "machine learning tag")
-    MlTag.create!(tag_id: ml_tag.id)
+      Setting["machine_learning.tags"] = true
 
-    proposal = create(:proposal, tag_list: [user_tag, ml_tag])
+      visit proposal_path(proposal)
 
-    visit proposal_path(proposal)
-
-    within "#tags_proposal_#{proposal.id}" do
-      expect(page).to have_link "user tag"
-      expect(page).not_to have_link "machine learning tag"
+      within "#tags_proposal_#{proposal.id}" do
+        expect(page).to have_link "user tag"
+        expect(page).to have_link "machine learning tag"
+      end
     end
 
-    Setting["machine_learning.tags"] = true
+    scenario "are displayed for budget investments if setting is enabled" do
+      create :ml_tag, tag: machine_learning_tag
+      investment.update! tag_list: [user_tag, machine_learning_tag]
 
-    visit proposal_path(proposal)
+      visit budget_investment_path(budget, investment)
 
-    within "#tags_proposal_#{proposal.id}" do
-      expect(page).to have_link "user tag"
-      expect(page).to have_link "machine learning tag"
+      within "#tags_budget_investment_#{investment.id}" do
+        expect(page).to have_link "user tag"
+        expect(page).not_to have_link "machine learning tag"
+      end
+
+      Setting["machine_learning.tags"] = true
+
+      visit budget_investment_path(budget, investment)
+
+      within "#tags_budget_investment_#{investment.id}" do
+        expect(page).to have_link "user tag"
+        expect(page).to have_link "machine learning tag"
+      end
     end
   end
 end
