e3d3761bab7b7b4487bf09d1c392e45b1fca16b3
Julian Herrero <microweb10@gmail.com>
2021-05-03 13:02:11 +0700

    Fix bug

--- app/models/tag_cloud.rb
+++ app/models/tag_cloud.rb
@@ -7,10 +7,10 @@ class TagCloud
   end
 
   def tags
-    default_tags = resource_model_scoped.last_week.tag_counts.
-                   where("lower(name) NOT IN (?)", category_names + geozone_names + default_blacklist)
-    default_tags = default_tags.where("id NOT IN (?)", ml_tags) unless Setting["machine_learning.tags"]
-    default_tags.order("#{table_name}_count": :desc, name: :asc).limit(10)
+    cloud_tags = resource_model_scoped.last_week.tag_counts.
+                     where("lower(name) NOT IN (?)", category_names + geozone_names + default_blacklist)
+    cloud_tags = cloud_tags.where.not(id: ml_tags) unless Setting["machine_learning.tags"]
+    cloud_tags.order("#{table_name}_count": :desc, name: :asc).limit(10)
   end
 
   def category_names
